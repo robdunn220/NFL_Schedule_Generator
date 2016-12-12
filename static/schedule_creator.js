5a601b724c174f989af1550d25522620bf57c83b
Robert Dunn <robertdunn220@gmail.com>
2016-12-12 15:17:59 -0500

    Fixed the bye-week generation, now correctly generates between week 4 and 12

--- static/schedule_creator.js
+++ static/schedule_creator.js
@@ -1,6 +1,7 @@
 var app = angular.module('nfl_app', ['ui.router', 'ngCookies']);
 
 app.factory("NFL_Api", function factoryFunction($http, $rootScope) {
+  // Creates an object for the returned results to be stored to
   var service = {};
   // Makes a call to server.py to return all NFL teams
   service.displayAllTeams = function() {
@@ -8,6 +9,7 @@ app.factory("NFL_Api", function factoryFunction($http, $rootScope) {
       url: '/api/all_teams'
     });
   };
+  // Returns the object after the call is made
   return service;
 });
 
@@ -49,15 +51,18 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
   $scope.schedule = [];
   $scope.weeksLeft = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+  // Makes a service call to return all teams. Results are stored in a $scope var, and passed on
   NFL_Api.displayAllTeams().success(function(res) {
     $scope.nflInfo = res;
     $scope.getTeamInfo($scope.nflInfo);
   });
 
+  // Function that takes the week and opponent name, and pushes it into the schedule array with keys
   $scope.addToSched = function(week, opp) {
     $scope.schedule.push({'week': week, 'opponent': opp});
   };
 
+  // Takes the user input, finds the team in the nflInfo object, and assigns that to the mainTeam var, which is the team the schedule will be created for
   $scope.getTeamInfo = function (nflInfo) {
     nflInfo.forEach(function(team) {
       if (team.team_name === $stateParams.team) {
@@ -67,6 +72,7 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
     });
   };
 
+  // Main schedule-generating function
   $scope.scheduleCreator = function(nflInfo) {
     var divisionOpponents = [];
     var otherDivisionOpponents = [];
@@ -80,6 +86,7 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
     var teamPicked;
     $scope.teams = [];
 
+    // Sorts the NFL teams out into arrays based on their relation to the selected team, mainTeam. The first if statement isolates mainTeam's divisional opponents. The first elseif finds the remainder of the teams in mainTeams conference and pushes them into an array. The last statement finds and pushes the other cobference teams to an array.
     nflInfo.forEach(function(team) {
       if ((team.division_name === $scope.mainTeam.division_name) && (team.team_name !== $scope.mainTeam.team_name)) {
         divisionOpponents.push(team);
@@ -94,11 +101,15 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
       }
     });
 
-    var byeRandNum = Math.floor(3 + Math.random() * 11);
-    weekPicked = $scope.weeksLeft[byeRandNum];
+    // First assignment to schedule. Picks a random number between 4 and 12 and assigns a bye to that week.
+    var byeRandNum = (Math.floor(Math.random() * 9) + 3);
+    console.log(byeRandNum);
+    console.log($scope.weeksLeft[byeRandNum]);
+    var byeWeekPicked = $scope.weeksLeft[byeRandNum];
     $scope.weeksLeft.splice(byeRandNum, 1);
-    $scope.addToSched(weekPicked, 'Bye');
+    $scope.addToSched(byeWeekPicked, 'Bye');
 
+    // Assignment of divisional opponents occurs here. Each divisional opponent is scheduled for 2 games. Need to figure out the best way to not have the same div. opponent scheduled subsequently
     divisionOpponents.forEach(function(team) {
       for (var i = 0; i < 2; i++) {
         randNum = (Math.floor(Math.random() * ($scope.weeksLeft.length - 1)));
@@ -108,6 +119,7 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
       }
     });
 
+    // Loops through the other conference divisions, and pushes the names to an array. One of these names will be randomly selected as the division that the other team will play against.
     for (var d = 0; d < 12; d+=4) {
       divisionNamesArray.push(otherDivisionOpponents[d].division_name);
     }
@@ -165,7 +177,7 @@ app.controller('ScheduleGeneratorController', function($scope, NFL_Api, $rootSco
 
     $scope.schedule.sort(compare);
 
-    $scope.teams[$scope.team_name] = new Schedule($scope.team_name, $scope.mainTeam.division_name, $scope.schedule);    
+    $scope.teams[$scope.team_name] = new Schedule($scope.team_name, $scope.mainTeam.division_name, $scope.schedule);
   };
 });
 
